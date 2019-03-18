48ba64ee8048c97c09df9d84808f6bb4677a282f
adammertel <mertel.adam@gmail.com>
2019-03-19 00:58:07 +0100

    gitignore

---
+++ processing/base.js
@@ -0,0 +1,77 @@
+var gdal = require("gdal");
+var turf = require("turf");
+var fs = require("fs");
+
+let lastTime = new Date().valueOf();
+module.exports.report = text => {
+  const now = new Date().valueOf();
+  console.log("task '" + text + "' finished in", (now - lastTime) / 1000, "s");
+  lastTime = new Date().valueOf();
+};
+
+const defaultRoundingPoint = 4;
+module.exports.round = round = (
+  numberToRound,
+  floatingPoint = defaultRoundingPoint
+) => {
+  return parseFloat(parseFloat(numberToRound).toFixed(floatingPoint));
+};
+
+const path = "./../data/";
+module.exports.readJSON = fileName => {
+  const dataset = JSON.parse(
+    fs.readFileSync(path + fileName + ".geojson", "utf8")
+  );
+  const fixedFeatures = dataset.features.map(f => {
+    const coordinates = f.geometry.coordinates.map(coords => {
+      if (!isNaN(coords)) {
+        return round(coords, 4);
+      } else {
+        return coords.map(cs => cs.map(c => round(c, 4)));
+      }
+    });
+    f.geometry.coordinates = coordinates;
+    return f;
+  });
+
+  dataset.features = fixedFeatures;
+
+  return dataset;
+};
+
+module.exports.saveFile = (name, data) => {
+  fs.writeFileSync(
+    path + name + ".geojson",
+    JSON.stringify(turf.featureCollection(data))
+  );
+};
+
+module.exports.equalPoints = equalPoints = (p1, p2) => {
+  const cs1 = p1.geometry.coordinates;
+  const cs2 = p2.geometry.coordinates;
+  return equalCoordinates(cs1, cs2);
+};
+
+module.exports.equalCoordinates = equalCoordinates = (cs1, cs2) => {
+  return cs1[0] === cs2[0] && cs1[1] === cs2[1];
+};
+
+module.exports.intersectingPoint = intersectingPoint = (e1, e2) => {
+  const ps1 = firstAndLastVertex(e1.geometry.coordinates);
+  const ps2 = firstAndLastVertex(e2.geometry.coordinates);
+
+  if (equalCoordinates(ps1[0], ps2[0]) || equalCoordinates(ps1[0], ps2[1])) {
+    return ps1[0];
+  } else if (
+    equalCoordinates(ps1[1], ps2[0]) ||
+    equalCoordinates(ps1[1], ps2[1])
+  ) {
+    return ps1[1];
+  } else {
+    return false;
+  }
+};
+
+module.exports.firstAndLastVertex = firstAndLastVertex = e => {
+  return [e[0], e[e.length - 1]];
+};
---
+++ processing/tobler.js
@@ -0,0 +1,30 @@
+var gdal = require("gdal");
+var turf = require("turf");
+
+var dataset = gdal.open("./../data/elevation/srtm.tif");
+const band = dataset.bands.get(1);
+const pixels = band.pixels;
+
+const gt = dataset.geoTransform;
+
+module.exports.value = value = (x, y) => {
+  const px = parseInt((x - gt[0]) / gt[1], 10);
+  const py = parseInt((y - gt[3]) / gt[5], 10);
+  return pixels.get(px, py);
+};
+
+// calculates the tobler coefficient for the given distance and height difference
+module.exports.toblerCoeff = toblerCoeff = (distance, diff) => {
+  return Math.pow(Math.E, -3.5 * Math.abs(diff / distance + 0.05));
+};
+
+// takes two points and returns calculated tobler coefficient
+module.exports.tobler = tobler = (p1, p2) => {
+  const h1 = value(p1[0], p1[1]);
+  const h2 = value(p2[0], p2[1]);
+
+  const diff = h1 - h2;
+  const dist = turf.distance(p1, p2) * 1000;
+
+  return toblerCoeff(dist, diff);
+};
